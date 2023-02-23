926b9c824bd1349dcd18dddc8984428967c165a3
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-02-23 17:44:18 +0100

    Revert "docs: Add group UML data model diagram"
    This reverts commit c6017f78778b709f2dcb2c4fc128b19f335e6576.

--- src/main/java/acme/roles/Consumer.java
+++ src/main/java/acme/roles/Consumer.java
@@ -26,14 +26,15 @@ public class Consumer extends AbstractRole {
 
 	// Serialisation identifier -----------------------------------------------
 
-	protected static final long	serialVersionUID	= 1L;
+	protected static final long serialVersionUID = 1L;
 
 	// Attributes -------------------------------------------------------------
-	@NotBlank
-	protected String			company;
 
 	@NotBlank
-	protected String			sector;
+	protected String company;
+
+	@NotBlank
+	protected String sector;
 
 	// Derived attributes -----------------------------------------------------
 
