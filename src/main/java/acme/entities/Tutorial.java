15280b1e7bb821cb1ffdef6ce95355bc9ea397a1
carlos-bermejo <carbersor@alum.us.es>
2023-03-05 13:41:33 +0100

    Add tutorial entity

--- src/main/java/acme/entities/Tutorial.java
+++ src/main/java/acme/entities/Tutorial.java
@@ -1,6 +1,47 @@
+
 package acme.entities;
 
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.roles.Assistant;
 
 public class Tutorial {
+	// Serialisation identifier -----------------------------------------------
 
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{1,3}[0-9][0-9]{3}")
+	protected String			code;
+
+	@NotBlank
+	@Length(min = 1, max = 75)
+	protected String			title;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			abstrac;
+
+	@NotBlank
+	@Length(min = 1, max = 100)
+	protected String			goals;
+
+	@Digits(integer = 3, fraction = 2)
+	protected double			estimatedHours;
+
+	// Derived attributes -----------------------------------------------------
+
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@ManyToOne(optional = false)
+	protected Assistant			assistant;
 }
