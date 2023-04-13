c1339a247f91c9a100a9c813801e8623f38d29d8
carlos-bermejo <carbersor@alum.us.es>
2023-04-14 00:28:11 +0200

    Add first versions of the CRUD classes

--- src/main/java/acme/features/assistant/tutorial/AssistantTutorialRepository.java
+++ src/main/java/acme/features/assistant/tutorial/AssistantTutorialRepository.java
@@ -7,7 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import acme.entities.tutorial.Tutorial;
+import acme.entities.tutorial.TutorialSession;
 import acme.framework.repositories.AbstractRepository;
+import acme.roles.Assistant;
 
 @Repository
 public interface AssistantTutorialRepository extends AbstractRepository {
@@ -20,5 +22,11 @@ public interface AssistantTutorialRepository extends AbstractRepository {
 
 	@Query("SELECT t FROM Tutorial t WHERE t.assistant.id = :id")
 	Collection<Tutorial> findTutorialsByAssistantId(int id);
+
+	@Query("SELECT s FROM Tutorial t JOIN TutorialSession s WHERE t.id = :id")
+	Collection<TutorialSession> findSessionsByTutorialId(int id);
+
+	@Query("SELECT a FROM Tutorial t JOIN Assistant a WHERE a.id = :activeRoleId")
+	Assistant findOneAssistantById(int activeRoleId);
 
 }
