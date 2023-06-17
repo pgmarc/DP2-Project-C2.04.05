57c78212b8f32c6040c83afb2ff609d1f14980d8
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-06-17 10:36:40 +0200

    feat: initial draft create offer for admin

--- src/main/java/acme/features/administrator/offer/AdministratorOfferRepository.java
+++ src/main/java/acme/features/administrator/offer/AdministratorOfferRepository.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import acme.entities.SystemCurrency;
 import acme.entities.offer.Offer;
 import acme.framework.repositories.AbstractRepository;
 
@@ -17,4 +18,7 @@ public interface AdministratorOfferRepository extends AbstractRepository {
 
 	@Query("SELECT offer FROM Offer offer WHERE offer.id = :offerId")
 	Offer findOfferById(int offerId);
+
+	@Query("SELECT sc FROM SystemCurrency sc")
+	SystemCurrency showSystemCurrency();
 }
