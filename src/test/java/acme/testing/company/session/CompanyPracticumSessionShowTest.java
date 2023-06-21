5814820f6e0e92d7deefcc0ce74b38730cc0357d
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-06-21 13:37:49 +0200

    feat: added show practicum sessions tests

--- src/test/java/acme/testing/company/session/CompanyPracticumSessionShowTest.java
+++ src/test/java/acme/testing/company/session/CompanyPracticumSessionShowTest.java
@@ -1,11 +1,13 @@
 
 package acme.testing.company.session;
 
-import org.junit.jupiter.api.Test;
+import java.util.Collection;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.practicum.PracticumSession;
 import acme.testing.TestHarness;
 
 public class CompanyPracticumSessionShowTest extends TestHarness {
@@ -15,38 +17,72 @@ public class CompanyPracticumSessionShowTest extends TestHarness {
 
 
 	@ParameterizedTest
-	@CsvFileSource(resources = "/company/practicum/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int recordIndex, final String code, final String title, final String practicumAbstract, final String goals, final String draftMode, final String startingDate, final String endingDate, final String estimatedTotalTime,
-		final String practicaPeriodLength) {
+	@CsvFileSource(resources = "/company/practicum-session/show-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test100Positive(final int practicumRecord, final int sessionRecord, final String title, final String sessionAbstract, final String startingDate, final String endingDate, final String moreInformation) {
 
 		super.signIn("company1", "company1");
 
 		super.clickOnMenu("Company", "Practica");
 		super.sortListing(0, "asc");
-		super.clickOnListingRecord(recordIndex);
+		super.clickOnListingRecord(practicumRecord);
 		super.checkFormExists();
+		super.clickOnButton("View scheduled sessions");
 
-		super.checkInputBoxHasValue("code", code);
+		super.clickOnListingRecord(sessionRecord);
 		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("practicumAbstract", practicumAbstract);
-		super.checkInputBoxHasValue("goals", goals);
-		super.checkInputBoxHasValue("draftMode", draftMode);
+		super.checkInputBoxHasValue("sessionAbstract", sessionAbstract);
 		super.checkInputBoxHasValue("startingDate", startingDate);
 		super.checkInputBoxHasValue("endingDate", endingDate);
-		super.checkInputBoxHasValue("estimatedTotalTime", estimatedTotalTime);
-		super.checkInputBoxHasValue("practicaPeriodLength", practicaPeriodLength);
+		super.checkInputBoxHasValue("moreInfo", moreInformation);
 
 		super.signOut();
 	}
 
-	@Test
-	public void test300HackingShouldNotShowPracticumDetailsNotCompanyRole() {
+	@ParameterizedTest
+	@CsvFileSource(resources = "/company/practicum-session/show-hacking.csv", encoding = "utf-8", numLinesToSkip = 1)
+	public void test300Hacking(final String username, final String practicumCode) {
+		Collection<PracticumSession> sessions;
+		String param;
+		sessions = this.repository.findPracticumSessionsByCompanyUsernameAndPracticumCode(username, practicumCode);
+		assert sessions.size() != 0;
 
-	}
+		for (final PracticumSession practicumSession : sessions) {
+			param = String.format("id=%d", practicumSession.getId());
 
-	@Test
-	public void test301HackingShouldNotShowPracticumDetailsOfOtherCompany() {
+			super.checkLinkExists("Sign in");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
 
+			super.signIn("administrator1", "administrator1");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("assistant1", "assistant1");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("auditor1", "auditor1");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("lecturer1", "lecturer1");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("student1", "student1");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+
+			super.signIn("company2", "company2");
+			super.request("/company/practicum-session/show", param);
+			super.checkPanicExists();
+			super.signOut();
+		}
 	}
 
 }
