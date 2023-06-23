03196e445a72c65381ecd02e4fd4151a24fba973
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-06-23 23:49:33 +0200

    feat: changes to classes

--- src/main/java/acme/entities/practicum/Practicum.java
+++ src/main/java/acme/entities/practicum/Practicum.java
@@ -29,7 +29,7 @@ public class Practicum extends AbstractEntity {
 
 	@Column(unique = true)
 	@NotBlank
-	@Pattern(regexp = "[A-Z]{1,3}[0-9]{3}")
+	@Pattern(regexp = "[A-Z]{1,3}\\d{3}")
 	protected String			code;
 
 	@NotBlank
