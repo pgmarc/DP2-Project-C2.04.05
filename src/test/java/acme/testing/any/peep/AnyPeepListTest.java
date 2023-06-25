222f742050634aea7576ce8f0c552a0de0c02b7c
Pedro González Marcos <pedgonmar2@alum.us.es>
2023-06-25 20:24:53 +0200

    feat: list peep test implemented

--- src/test/java/acme/testing/any/peep/AnyPeepListTest.java
+++ src/test/java/acme/testing/any/peep/AnyPeepListTest.java
@@ -11,15 +11,24 @@ public class AnyPeepListTest extends TestHarness {
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/peep/list-positive.csv", encoding = "utf-8", numLinesToSkip = 1)
-	public void test100Positive(final int recordIndex, final String instantiationMoment, final String nick, final String title) {
+	public void test100Positive(final int recordIndex, final String instantiationMoment, final String title, final String nick, final String message, final String email, final String moreInfo) {
 
-		super.clickOnMenu("Peeps");
+		super.clickOnMenu("Anonymous", "List peeps");
 		super.checkListingExists();
 		super.sortListing(0, "asc");
 
-		super.checkColumnHasValue(recordIndex, 0, instantiationMoment);
-		super.checkColumnHasValue(recordIndex, 1, title);
-		super.checkColumnHasValue(recordIndex, 1, title);
+		super.checkColumnHasValue(recordIndex, 0, nick);
+		super.checkColumnHasValue(recordIndex, 1, instantiationMoment);
+		super.checkColumnHasValue(recordIndex, 2, title);
+
+		super.clickOnListingRecord(recordIndex);
+		super.checkFormExists();
+		super.checkInputBoxHasValue("instantiationMoment", instantiationMoment);
+		super.checkInputBoxHasValue("title", title);
+		super.checkInputBoxHasValue("nick", nick);
+		super.checkInputBoxHasValue("message", message);
+		super.checkInputBoxHasValue("email", email);
+		super.checkInputBoxHasValue("moreInfo", moreInfo);
 
 	}
 
@@ -28,51 +37,45 @@ public class AnyPeepListTest extends TestHarness {
 	@Test
 	public void test101Positive() {
 
-		super.checkLinkExists("Sign in");
-		super.request("/any/peep/list");
-		super.checkListingExists();
-		super.request("/any/peep/publish");
-		super.checkFormExists();
-
 		super.signIn("administrator1", "administrator1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 
 		super.signIn("assistant1", "assistant1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 
 		super.signIn("auditor1", "auditor1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 
 		super.signIn("company1", "company1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 
 		super.signIn("lecturer1", "lecturer1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 
 		super.signIn("student1", "student1");
-		super.request("/any/peep/list");
+		super.clickOnMenu("Authenticated", "List peeps");
 		super.checkListingExists();
-		super.request("/any/peep/publish");
+		super.clickOnButton("Create peep");
 		super.checkFormExists();
 		super.signOut();
 	}
